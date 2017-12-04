70661614893cdc6de9ab48e76f7585839c545368
Neil McGlohon <nmcglohon@gmail.com>
2017-12-04 17:49:03 -0500

    Day 1: Puzzle 2 Complete

--- Day1/inverseCaptcha.py
+++ Day1/inverseCaptcha.py
@@ -1,22 +1,45 @@
 import numpy as np
 
 
-inputString = ''
-with open('input.txt') as f:
-    inputString = f.readline()
-inputString = inputString.strip()
+def getInputArray(filename):
+    inputString = ''
+    with open(filename) as f:
+        inputString = f.readline()
+    inputString = inputString.strip()
 
-inputArray = np.zeros(len(inputString)+1)
-for i,char in enumerate(inputString):
-    if(char != '\n'):
-        inputArray[i] = int(char)
-inputArray[-1] = inputArray[0]
+    inputArray = np.zeros(len(inputString))
+    for i,char in enumerate(inputString):
+        if(char != '\n'):
+            inputArray[i] = int(char)
 
-totalSum = 0
-lastItem = -1
-for item in inputArray:
-    if item == lastItem:
-        totalSum += item
-    lastItem = item
+    return inputArray
 
-print(totalSum)
+def solvePartOne(inputArray):
+    inputArray = np.append(inputArray, inputArray[0])
+    totalSum = 0
+    lastItem = -1
+    for item in inputArray:
+        if item == lastItem:
+            totalSum += item
+        lastItem = item
+    return totalSum
+
+
+def solvePartTwo(inputArray):
+    stepsAhead = int(len(inputArray)/2)
+    totalSum = 0
+    for i,item in enumerate(inputArray):
+        otherItem = inputArray[(i+stepsAhead)%len(inputArray)]
+        if item == otherItem:
+            totalSum += item
+    return totalSum
+
+
+def main():
+    inputArray = getInputArray('input.txt')
+    print("Part 1: %d"%solvePartOne(inputArray.copy()))
+    print("Part 2: %d"%solvePartTwo(inputArray.copy()))
+
+
+if __name__ == '__main__':
+    main()
