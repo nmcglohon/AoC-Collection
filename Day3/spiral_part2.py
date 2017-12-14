d74ff0bd55a3b70ec9ad38ddd74af5f689472c44
Neil McGlohon <nmcglohon@gmail.com>
2017-12-14 01:27:24 -0500

    Day 3: Part 2 Complete

--- Day3/spiral_part2.py
+++ Day3/spiral_part2.py
@@ -1,38 +1,111 @@
 import sys
 import math
+from enum import Enum
 from spiral import solvePartOne, getBottomRightValue
+
+class Direction(Enum):
+    RIGHT = 0
+    UP = 1
+    LEFT = 2
+    DOWN = 3
 
 class GameBoard:
     def __init__(self, sideLen, inputVal):
-        self.NodeArray = [x[:] for x in [[0] * sideLen] * sideLen]
+        self.sideLen = sideLen
+        self.NodeArray = [x[:] for x in [[0] * (sideLen)] * (sideLen)] #initialize one in either direction
         self.origin = (int(sideLen/2),int(sideLen/2))
 
         self.NodeArray[self.origin[0]][self.origin[1]] = 1
         self.numAllocated = 1
         self.lastAllocatedCoord = self.origin
         self.inputVal = inputVal
+        self.direction = Direction.RIGHT
 
     def __str__(self):
-        return(str(self.NodeArray))
+        return "GameBoard: [%d x %d]; %d allocated; last allocated coord: %s; inputVal=%d"%(self.sideLen, self.sideLen, self.numAllocated, str(self.lastAllocatedCoord), self.inputVal)
 
-    def __getNextCoords(self):
-        pass
+    def printGrid(self):
+        for row in reversed(self.NodeArray):
+            print(row)
+
+    def __getNextCoord(self):
+        if self.lastAllocatedCoord == self.origin:
+            return (self.origin[0]+1, self.origin[1])
+
+        elif self.direction is Direction.RIGHT:
+            if self.getNeighborValue(self.lastAllocatedCoord, Direction.UP) > 0:
+                return (self.lastAllocatedCoord[0]+1,self.lastAllocatedCoord[1])
+            else:
+                self.direction = Direction.UP
+                return (self.lastAllocatedCoord[0],self.lastAllocatedCoord[1]+1)
+
+        elif self.direction is Direction.UP:
+            if self.getNeighborValue(self.lastAllocatedCoord, Direction.LEFT) > 0:
+                return (self.lastAllocatedCoord[0],self.lastAllocatedCoord[1]+1)
+            else:
+                self.direction = Direction.LEFT
+                return (self.lastAllocatedCoord[0]-1,self.lastAllocatedCoord[1])
+
+        elif self.direction is Direction.LEFT:
+            if self.getNeighborValue(self.lastAllocatedCoord, Direction.DOWN) > 0:
+                return (self.lastAllocatedCoord[0]-1,self.lastAllocatedCoord[1])
+            else:
+                self.direction = Direction.DOWN
+                return (self.lastAllocatedCoord[0],self.lastAllocatedCoord[1]-1)
+
+        elif self.direction is Direction.DOWN:
+            if self.getNeighborValue(self.lastAllocatedCoord, Direction.RIGHT) > 0:
+                return (self.lastAllocatedCoord[0],self.lastAllocatedCoord[1]-1)
+            else:
+                self.direction = Direction.RIGHT
+                return (self.lastAllocatedCoord[0]+1,self.lastAllocatedCoord[1])
+
+    def __getNextVal(self,nextCoord):
+        N = self.getNeighborsCoords(nextCoord)
+
+        totalSum = 0
+        for coord in N:
+            try:
+                totalSum += self.NodeArray[coord[1]][coord[0]]
+            except:
+                pass
+        return totalSum
+
+    def getNeighborsCoords(self,coords):
+        N = [(i,j) for i in range(coords[0]-1,coords[0]+2) for j in range(coords[1]-1,coords[1]+2) if not (i is coords[0] and j is coords[1])]
+        return N
+
+    def getNeighborValue(self,coord,direction):
+        try:
+            if direction == Direction.RIGHT:
+                return self.NodeArray[coord[1]][coord[0]+1]
+            if direction == Direction.UP:
+                return self.NodeArray[coord[1]+1][coord[0]]
+            if direction == Direction.LEFT:
+                return self.NodeArray[coord[1]][coord[0]-1]
+            if direction == Direction.DOWN:
+                return self.NodeArray[coord[1]-1][coord[0]]
+        except:
+            return 0
+
+    def __allocate(self,x,y,val):
+        self.NodeArray[y][x] = val
+        self.numAllocated += 1
+        self.lastAllocatedCoord = (x,y)
 
     def allocateNext(self):
-        pass
-
+        nextCoord = self.__getNextCoord()
+        val = self.__getNextVal(nextCoord)
+        self.__allocate(nextCoord[0],nextCoord[1],val)
+        return val
+        
     def findAnswer(self):
-        pass
+        curVal = 0
+        while curVal < self.inputVal:
+            curVal = self.allocateNext()
 
-class Node:
-    def __init__(self,x,y,init_dat):
-        self.x = x
-        self.y = y
-        self.dat = init_dat
+        return curVal
 
-    def getNeighborsCoords(self):
-        N = [(i,j) for i in range(self.x-1,self.x+2) for j in range(self.y-1,self.y+2) if not (i is self.x and j is self.y)]
-        return N
 
 def solvePartTwo(inputVal):
     bottomRightVal = getBottomRightValue(inputVal)
