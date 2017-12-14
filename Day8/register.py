8e7df98971256d3fd458584daba38d808603479b
Neil McGlohon <nmcglohon@gmail.com>
2017-12-14 18:51:25 -0500

    Day 8: Part 2 Complete

--- Day8/register.py
+++ Day8/register.py
@@ -7,24 +7,33 @@ def parseInput(inFilename):
             lines.append(line.strip())
     return lines
 
-
-def solvePartOne(lines):
+def evaluateInstructions(lines, isFindHighest):
     registers = defaultdict(int)
+    maxVal = 0
     for line in lines:
         (name, incdec, number, cond, cond_name, op, cond_number) = line.split(' ')
         if eval("registers[cond_name] " + op + cond_number):
             if incdec == 'inc':
                 registers[name] += int(number)
+                if registers[name] > maxVal:
+                    maxVal = registers[name]
             elif incdec == 'dec':
                 registers[name] -= int(number)
+                if registers[name] > maxVal:
+                    maxVal = registers[name]
             else:
                 raise Exception("Invalid increment or decrement operator")
-    return max(registers.values())
 
+    if not isFindHighest: #part 1
+        return max(registers.values())
+    else: #part 2
+        return maxVal
 
-def solvePartTwo(progTree):
-    pass
+def solvePartOne(lines):
+    return(evaluateInstructions(lines,False))
 
+def solvePartTwo(lines):
+    return(evaluateInstructions(lines,True))
 
 def main():
     lines = parseInput('input.txt')
