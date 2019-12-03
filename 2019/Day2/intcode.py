5ff745e091219b00f3c6e49dd2c73c96889e089e
Neil McGlohon <nmcglohon@gmail.com>
2019-12-03 00:42:16 -0500

    2019 Day 2 Part 2: Complete

--- 2019/Day2/intcode.py
+++ 2019/Day2/intcode.py
@@ -12,8 +12,11 @@ def opcode2(intcode, pos):
 
     intcode[intcode[pos+3]] = operand1 * operand2
 
+def reset_state(intcode, noun, verb):
+    intcode[1] = noun
+    intcode[2] = verb
+
 def execute_program(intcode):
-    print(intcode)
     cur_opcode = -1
     cur_pos = 0
     while cur_opcode is not 99:
@@ -26,6 +29,8 @@ def execute_program(intcode):
             exit("Error: Invalid Opcode Found")
         
         cur_pos += 4
+    
+    return intcode[0]
 
 def part1():
 
@@ -41,7 +46,26 @@ def part1():
 
     execute_program(program)
 
-    print("Value at position 0 at program halt is: %d"%program[0])
+    print("Part 1: Value at position 0 at program halt is: %d"%program[0])
+
+def part2():
+    with open("input.txt") as f:
+        program_text = f.readline()
+
+    program = program_text.split(',')
+    original_program = list(map(int,program))
+
+    noun_verb_pairs = [(i,j) for i in range(0,100) for j in range(0,100)]
+
+    for pair in noun_verb_pairs:
+        program = list.copy(original_program)
+        reset_state(program, pair[0], pair[1])
+        ret = execute_program(program)
+
+        if ret == 19690720:
+            amalgam = 100 * pair[0] + pair[1]
+            print("Part 2: Correct Input Amalgam = %d"%amalgam)
 
 if __name__ == "__main__":
     part1()
+    part2()
