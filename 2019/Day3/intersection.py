2f5e85767d0bdc0de65d95ade0eea61933e87a21
Neil McGlohon <nmcglohon@gmail.com>
2019-12-03 02:51:09 -0500

    2019 Day 3 Part 2: Complete

--- 2019/Day3/intersection.py
+++ 2019/Day3/intersection.py
@@ -67,8 +67,6 @@ def process_wire(grid, origin_coord, wire_path, wire_identifier):
         else:
             exit("Bad instruction direction")
 
-
-
 def get_closest_intersection_dist_to_coordinates(grid, coords):
     min_dist = 9999999
     for int_coords in global_intersection_storage:
@@ -94,6 +92,55 @@ def part1():
 
     print("Part 1: Minimum distance of intersection to central port: %d"%closest_distance)
 
+#realized how dumb and overcomlicating I was being with this, just create sets of coords that each wire has visited, recording the lengths it took to get to those points for each
+def get_coords(wire_path):
+    length = 0
+    path_dict = {}
+    cur_x, cur_y = 0,0
+
+    for instruction in wire_path:
+        instruction_direction = instruction[0]
+        instruction_distance = int(instruction[1:])
+
+        for i in range(1, instruction_distance+1):
+            length += 1
+            if instruction_direction == "R":
+                point = (cur_x + 1, cur_y)
+                if point not in path_dict:
+                    path_dict[point] = length
+                cur_x += 1
+            elif instruction_direction == "L":
+                point = (cur_x - 1, cur_y)
+                if point not in path_dict:
+                    path_dict[point] = length
+                cur_x -= 1
+            elif instruction_direction == "D":
+                point = (cur_x, cur_y + 1)
+                if point not in path_dict:
+                    path_dict[point] = length
+                cur_y += 1
+            elif instruction_direction == "U":
+                point = (cur_x, cur_y - 1)
+                if point not in path_dict:
+                    path_dict[point] = length
+                cur_y -= 1
+
+    return path_dict
+
+def part2():
+    with open("input.txt") as f:
+        wire_path_1 = f.readline().split(',')
+        wire_path_2 = f.readline().split(',')
+
+    wire1_path_dict = get_coords(wire_path_1)
+    wire2_path_dict = get_coords(wire_path_2)
+
+    points_of_intersection = set.intersection(set(wire1_path_dict.keys()),set(wire2_path_dict.keys()))
+    
+    # min_distance = min([abs(x) + abs(y) for (x,y) in points_of_intersection]) #part 1 redux for good measure to show how much better this is
+    min_latency = min([wire1_path_dict[intersection] + wire2_path_dict[intersection] for intersection in points_of_intersection])
+    print("Part 2: Minimum latency of intersection to central port: %d"%min_latency)
 
 if __name__ == "__main__":
     part1()
+    part2()
